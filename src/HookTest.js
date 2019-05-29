132bb5b5822861d229dd976f39347d6bbe3dd761
qiaoxu <qiaoxu@mtedu.com>
2019-05-29 15:05:47 +0800

    实现action以及FormItem

--- src/HookTest.js
+++ src/HookTest.js
@@ -13,11 +13,11 @@ function FruitList({fruits, setFruit}) {
 function FruitAdd(props) {
     const [pname, setPname] = useState('草莓');
 
-    const {dispatch}=useContext(Context);
+    const {dispatch} = useContext(Context);
 
     const onAddFruit = (e) => {
         if (e.key === 'Enter') {
-            dispatch({type:'add',payload:pname })
+            dispatch({type: 'add', payload: pname})
             //props.onAddFruit(pname);
             //清空输入框
             setPname('');
@@ -34,16 +34,52 @@ function FruitAdd(props) {
 function fruitReducer(state, action) {
     switch (action.type) {
         case 'init':
-            return action.payload;
+            return {...state, list: action.payload};
         case'add':
-            return [...state, action.payload];
+            return {...state, list: [...state, action.payload]};
+        case'loading_start':
+            return {...state, loading: true};
+        case'loading_end':
+            return {...state, loading: false}
         default:
             return state;
     }
 }
 
+//判断对象是否是promise
+function isPromise(obj) {
+    return (
+        !!obj && (typeof obj === 'object' || typeof obj === 'function') && typeof obj.then === 'function'
+    )
+}
+
+//mock一个异步方法
+async function asyncFetch(p) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(p);
+        }, 1000)
+    })
+}
+
+
+//对dispatch函数进行封装，使其支持处理异步数据
+function wrapDispatch(dispatch) {
+    return function (action) {
+        if (isPromise(action.payload)) {
+            dispatch({type: 'loading_start'});
+            action.payload.then(v => {
+                dispatch({type: action.type, payload: v});
+                dispatch({type: 'loading_end'})
+            })
+        } else {
+            dispatch(action)
+        }
+    }
+}
+
 //创建上下文
-const Context=React.createContext();
+const Context = React.createContext();
 
 export default function HookTest() {
     //useState参数是状态初始值
@@ -52,16 +88,25 @@ export default function HookTest() {
     //const [fruits, setFruits] = useState([]);
 
     //参数1是相关reducer，参数2是初始值
-    const [fruits, dispatch] = useReducer(fruitReducer, []);
+    //const [fruits, dispatch] = useReducer(fruitReducer, []);
+    const [{list: fruits, loading}, originDispatch] = useReducer(fruitReducer, {
+        list: [],
+        loading: false
+    })
+
+    //包装dispatch
+    const dispatch=wrapDispatch(originDispatch);
+
     //使用useEffect操作副作用，第一个参是回调函数，第二个参是数组
     //任何的状态变更都会执行useEffect
     //请务必设置依赖选项，如果没有则设置空数组表示仅执行一次
     useEffect(() => {
         console.log('get Fruits');
-        setTimeout(() => {
-            //setFruits(['草莓', '香蕉'])
-            dispatch({type: 'init', payload: ['草莓', '香蕉']})
-        }, 2000)
+        dispatch({type: 'init', payload: asyncFetch(['草莓', '香蕉'])})
+        // setTimeout(() => {
+        //     //setFruits(['草莓', '香蕉'])
+        //     dispatch({type: 'init', payload: ['草莓', '香蕉']})
+        // }, 2000)
     }, []);
 
     //设置依赖选项
@@ -84,14 +129,18 @@ export default function HookTest() {
 
     return (
         //提供上下文的值
-        <Context.Provider value={{fruits,dispatch}}>
+        <Context.Provider value={{fruits, dispatch}}>
             <div>
                 <p>{fruit === '' ? "请选择喜爱的水果" : `您选择的是${fruit}`}</p>
 
                 {/*<FruitAdd onAddFruit={pname => dispatch({type: 'add', payload: pname})}/>*/}
                 {/* 这里不再需要给FruitAdd传递变更函数，实现了解耦 */}
                 <FruitAdd/>
-                <FruitList fruits={fruits} setFruit={setFruit}/>
+
+                {/*加载状态处理*/}
+                {
+                    loading?(<div>数据加载中...</div>):(<FruitList fruits={fruits} setFruit={setFruit}/>)
+                }
             </div>
         </Context.Provider>
     )
--- src/KFormTest.js
+++ src/KFormTest.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import {Icon} from 'antd';
 
 //高阶组件：扩展现有表单，提供控件包装、事件处理、表单校验
 function kFormCreate(Comp) {
@@ -55,6 +55,21 @@ function kFormCreate(Comp) {
             cb(ret, this.state);
         }
 
+        //焦点处理，错误获取等
+        handleFocus=e=>{
+            const field=e.target.name;
+            this.setState({
+                [field+'Focus']:true
+            })
+        }
+
+        isFieldTouched=field=>{
+            return !!this.state[field+'Focus']//双！！是转成bool值
+        }
+
+        getFieldError=field=>{
+            return this.state[field+'Message']
+        }
         //包装函数：接收字段名和校验选项返回一个高阶组件
         getFieldDec = (field, option) => {
             this.options[field] = option;//传进来要校验的项
@@ -64,7 +79,8 @@ function kFormCreate(Comp) {
                         React.cloneElement(InputComp, {
                             name: field,
                             value: this.state[field] || '',
-                            onChange: this.handleChange//执行校验设置状态等
+                            onChange: this.handleChange,//执行校验设置状态等
+                            onFocus:this.handleFocus//input获取焦点
                         })
                     }
                 </div>
@@ -73,40 +89,79 @@ function kFormCreate(Comp) {
 
         render() {
             return (
-                <Comp getFieldDec={this.getFieldDec} validateFields={this.validateFields}/>
+                <Comp
+                    getFieldDec={this.getFieldDec}
+                    validateFields={this.validateFields}
+                    isFieldTouched={this.isFieldTouched}
+                    getFieldError={this.getFieldError}
+                />
             )
         }
 
     }
 }
 
+//FormItem组件
+class FormItem extends Component {
+    render() {
+        return (
+            <div>
+                {/*默认插槽*/}
+                {this.props.children}
+                {this.props.help && (
+                    <p style={{color: this.props.validateStatus == 'error' ? 'red' : 'green'}}>
+                        {this.props.help}
+                    </p>
+                )}
+            </div>
+        )
+    }
+}
+
+//KInput组件
+class KInput extends Component{
+    render(){
+        console.log(this.props);
+        const {prefix,...rest}=this.props;
+        return(
+            <div>
+                {prefix}
+                <input {...rest}/>
+            </div>
+        )
+    }
+}
+
 @kFormCreate
 class KFormTest extends Component {
-    onSubmit=()=>{
-        this.props.validateFields((isValid,values)=>{
-            if(isValid) {
+    onSubmit = () => {
+        this.props.validateFields((isValid, values) => {
+            if (isValid) {
                 console.log(values);
                 alert('登录成功！')
-            }else {
+            } else {
                 console.log(values);
                 alert('校验失败！')
             }
         })
     }
+
     render() {
-        const {getFieldDec} = this.props;
+        const {getFieldDec,isFieldTouched,getFieldError} = this.props;
+        const usernameError=isFieldTouched('username') && getFieldError('username');
+        const passwordError=isFieldTouched('password') && getFieldError('password');
         return (
             <div>
-                <div>
-                    {getFieldDec('username',{
-                        rules:[{ required: true, message: "Please input your username!" }]
-                    })(<input type='text'/>)}
-                </div>
-                <div>
-                    {getFieldDec('password',{
-                        rules:[{ required: true, message: "Please input your Password!" }]
-                    })(<input type='password'/>)}
-                </div>
+                <FormItem validateStatus='error' help={usernameError}>
+                    {getFieldDec('username', {
+                        rules: [{required: true, message: "Please input your username!"}]
+                    })(<KInput type='text' prefix={<Icon type='user'/>}/>)}
+                </FormItem>
+                <FormItem validateStatus='error' help={passwordError}>
+                    {getFieldDec('password', {
+                        rules: [{required: true, message: "Please input your Password!"}]
+                    })(<KInput type='password' prefix={<Icon type='lock'/>}/>)}
+                </FormItem>
 
                 <button onClick={this.onSubmit}>登录</button>
             </div>
